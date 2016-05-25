3669843966119597bc6a0bbc94384985e857ad30
xjxb21 <xiaojixiang99@163.com>
2016-05-25 15:54:22 +0800

    Main jdbc

--- src/main/java/com/bigvideo/demo/controller/ThymeleafDemo.java
+++ src/main/java/com/bigvideo/demo/controller/ThymeleafDemo.java
@@ -1,11 +1,12 @@
 package com.bigvideo.demo.controller;
 
+import com.bigvideo.demo.beans.DbInfo;
 import com.bigvideo.demo.beans.Person;
+import com.bigvideo.demo.service.DemoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.Map;
 
 /**
  * Created by xiao on 2016/5/23.
@@ -14,11 +15,22 @@ import java.util.Map;
 @Controller
 public class ThymeleafDemo {
 
+    @Autowired
+    DemoService demoService;
+
     @RequestMapping(value = "/demoThymeleaf")
     public String demoThymeleaf(Model model) {
         Person single = new Person("bigvideo", 2);
         model.addAttribute("single", single);
-        //map.put("single", single);
-        return "index2";
+        return "ThymeleafDemoHtml";
+    }
+
+    @RequestMapping(value = "/TestConnection")
+    public String testConnection(Model model) {
+        String dbSysTime = demoService.getDbSysTime();
+        DbInfo dbInfo = new DbInfo();
+        dbInfo.setDbDate(dbSysTime);
+        model.addAttribute("dbInfo", dbInfo);
+        return "JDBCdemo";
     }
 }
